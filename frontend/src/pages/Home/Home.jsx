fe626a68bce77c6bdbe9549f9ef2f296c58daf1c
shrish-29331 <156535939+shrish-29331@users.noreply.github.com>
2025-06-14 23:59:39 +0530

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import Header from '../../components/header/Header';
+import Header from '../../components/Header/Header';
 import './Home.css';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 import MerchantList from '../../components/MerchantList/MerchantList'; // ⬅️ New import
